85dc4b3c87c93c60f5676432fbc17bb8ac7d57c4
guxinting <gu111777@163.com>
2016-07-14 14:17:47 +0800

    test by guxinting 20160714

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # git_test
 just test git config
+
+add a modem file .c
+add a folder
+
---
+++ folder/mtk_cam_rules.h
@@ -0,0 +1,28 @@
+SH55TIANYICF8090
+
+	938165	[SIM_DRV]Check HW WAveform for HW issue. TS:0 T0:0 usim_dcb->ev_status:fffffffc, int_status:2
+	
+	
+	
+	
+adb shell "echo \"--update_package=/sdcard/update.zip\" > /cache/recovery/c      
+
+[FAQ05993]»Á∫Œ≈‰÷√MT658x/7x/9x cameraµƒfeature table
+
+CUSTOM_HAL_IMGSENSOR = ov2685_mipi_yuv sp0a19_yuv sp2529_mipi_yuv gc2145mipi_yuv sp0a10_yuv gc0329_yuv 
+
+#define OV2685MIPI_SENSOR_ID                    0x2685
+#define SENSOR_DRVNAME_OV2685_MIPI_YUV          "ov2685mipiyuv"
+ov2685_mipi_yuv
+config.ftbl.ov2685_mipi_yuv.h
+SENSOR_DRVNAME_OV2685_MIPI_YUV
+
+#define SP2529_SENSOR_ID                         0x0025
+#define SENSOR_DRVNAME_SP2529_YUV    	"sp2529yuv"
+sp2529_mipi_yuv
+config.ftbl.sp2529_yuv.h
+SENSOR_DRVNAME_SP2529_YUV
+
+
+#define SENSOR_DRVNAME_S5K4ECGX_MIPI_YUV        "s5k4ecgxmipiyuv"
+#define S5K4ECGX_SENSOR_ID                      0x4EC0                                                                                                                                                                                                                                                                                                                                                                                                                                     ommand"
